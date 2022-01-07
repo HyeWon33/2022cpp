1de62c52816b61f7fcda30b2c78891600e678b0a
hjw141@naver.com <hjw141@naver.com>
2022-01-07 15:02:14 +0900

    220107

--- 220107_5/23_virtual.cpp
+++ 220107_5/23_virtual.cpp
@@ -15,6 +15,9 @@ public:
     void Cry() override {cout << "Dog Cry" << endl;}
 };
 
+// 1. 클래스가 가상 함수를 1개라도 소유학 있다면, 객체 크기가 8바이트(포인터 크기)가 증가한다.
+//  > 객체 내부에 가상 함수 테이블을 가르키는 포인터가 추가된다.
+
 int main(){
     Animal a; 
     cout << sizeof(a) << endl; //기본 : 4 -> virtual : 16
